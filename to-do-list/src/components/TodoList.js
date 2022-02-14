24db869799ef6de58a2d57cc81d8dc76a22345b3
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-02-14 15:02:43 +0100

    Create the form

---
+++ to-do-list/src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+function TodoList() {
+const [todos, setTodos] = useState([]) ;
+
+const addTodo = todo => {
+    
+}
+
+  return (
+    <div>
+        <h1>What's the plan for Today ?</h1>
+        <TodoForm />
+    </div>
+  );
+}
+
+export default TodoList
